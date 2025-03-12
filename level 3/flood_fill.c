3d6c423038224a6d751bbaae56fcd7bdd00ab89d
Rida Maanane <rmaanane@e2r5p12.1337.ma>
2025-03-12 11:31:55 +0000

    testing is everything

--- level 3/flood_fill.c
+++ level 3/flood_fill.c
@@ -7,7 +7,7 @@ typedef struct  s_point
 void fill(char **tab, t_point size, t_point current, char to_fill)
 {
     if (current.y < 0 || current.x < 0 || current.y >= size.y || current.x >= size.x || tab[current.y][current.x] != to_fill)
-        return;
+        return ;
     
     tab[current.y][current.x] = 'V';  
 
